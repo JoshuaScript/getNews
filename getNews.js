4eb7da112d446be75e9999093119e395f94ae51e
Joshua Rabiu <38544454+JoshuaScript@users.noreply.github.com>
2019-09-23 13:37:09 -0500

    Refactored - using more clean/expressive code.

--- getNews.js
+++ getNews.js
@@ -1,20 +1,10 @@
-/**
- * This webtask grabs the latest headlines from Reuters & outputs them in a table in which they are sorted by category
- * Usage: curl https://wt-e63b63f6cf3cc11e64f3e24f6f56b98a-0.sandbox.auth0-extend.com/getNews
- * The webtask url may also be set to an environment variable in order to make the synatax more terse. 
- *   For example: $ export news=https://wt-e63b63f6cf3cc11e64f3e24f6f56b98a-0.sandbox.auth0-extend.com/getNews
-       Once this is done, the endpoint can be called with more concise syntax (curl $news)
- */
-
-const Table = require('cli-table3');
-const x = require('x-ray')({
-  filters: {
-    clean: value => {
-      // removes the line break characters that were showing up in the headlines
-      return value.replace(/[\r\n]/g, ' ').trim();
+const 
+  Table = require('cli-table3'),
+  x = require('x-ray')({
+    filters: {
+      clean: value => value.replace(/[\r\n]/g, ' ').trim() // removes newline characters
     }
-  }
-});
+  });
 
 /**
 * @param context {WebtaskContext}
@@ -23,36 +13,30 @@ module.exports = (context, req, res) => {
   x('https://www.reuters.com/', '.module-news-headline-row', [
     {
       title: '.title-last',
-      titleLink: 'h4 a@href',
-      headlines: [ '.story-title | clean' ],
-      headlineLinks: [ '.story-title a@href' ]
+      headlines: [ '.story-title | clean' ]
     }
-  ]).then(data => {
-      const titleArr = [];
-      for (let i = 0; i < 3; i++) {
-        titleArr.push(data[i].title);
-      }
-      const table = new Table({
-        head: titleArr,
-        colWidths: [68, 68]
+  ])
+  .then(data => {
+    const 
+      head = []
+      rowOne = [],
+      rowTwo = [],
+      rowThree = [],
+      table = new Table({
+        head,
+        colWidths: [ 68, 68 ]
       });
-    
-      const rowOne = [];
-      const rowTwo = [];
-      const rowThree = [];
-      for (let i = 0; i < 3; i++) {
-        rowOne.push(data[i].headlines[0]);
-        rowTwo.push(data[i].headlines[1]);
-        rowThree.push(data[i].headlines[2]);
-      }
-      
-      table.push(rowOne);
-      table.push(rowTwo);
-      table.push(rowThree);
-
-      res.writeHead(200, { 'Content-Type': 'text/plain ' });
-      res.write('\n Latest headlines from Reuters: \n');
-      res.write(table.toString());
-      res.end();
+  
+    data.slice(0, 3).forEach(({ title , headlines: [ firstHeadline, secondHeadline, thirdHeadline ] }) => {
+      head.push(title);
+      rowOne.push(firstHeadline);
+      rowTwo.push(secondHeadline);
+      rowThree.push(thirdHeadline);
     });
-  };
+
+    table.push(rowOne, rowTwo, rowThree);
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Latest headlines from Reuters: \n ${String(table)}`);
+  });
+}
